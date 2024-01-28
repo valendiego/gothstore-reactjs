3ccb4a29acd5b482c81641349646bc32092de97f
Valentina Diego <valentinadiego90@gmail.com>
2024-01-28 18:24:42 -0300

    Se corrigió la ruta de itemdetailcontainer

--- gothstore/src/App.jsx
+++ gothstore/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
             <Routes>
               <Route path='/' element={<ItemListContainer />} />
               <Route path='/category/:categoryId' element={<ItemListContainer />} />
-              <Route path='/detail/:productId' element={<ItemDetailContainer />} />
+              <Route path='/item/:productId' element={<ItemDetailContainer />} />
               <Route path='*' element={<h1>404 NOT FOUND</h1>} />
             </Routes>
           </CartProvider>
